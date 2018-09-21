42ec92a301f9efac514152c17277ce78bc3c3440
Valentine-Efagene <efagenevalentine@gmail.com>
2018-09-21 15:02:39 +0100

    update

---
+++ Test/src/test/Test.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * @author valentyne
+ */
+public class Test {
+    public static void main(String[] args) {
+        Path path = Paths.get("..\\");
+        
+        if (Files.exists(path)){
+            print("Exists");
+            
+            if(Files.isDirectory(path)){
+                print("It is a directory");
+                try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);){
+                    for(Path p:directoryStream){
+                        print(p);
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }else{
+            print("Does not exist");
+        }
+        
+        System.out.println(path.toAbsolutePath());
+    }
+    
+    
+    public static <T> void print( T str) {
+        System.out.println(str);
+    }
+}
